c36a86e2180d0f6f1d96ed3650d6f3804d992563
monpy <4696monpy@gmail.com>
2014-09-14 20:14:34 +0900

    ステージからラインを操作する。

--- Assets/Scripts/Stage.cs
+++ Assets/Scripts/Stage.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Stage : MonoBehaviour {
 
+	public GameObject line;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +13,29 @@ public class Stage : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void StageRoutinMethod(){
+		AllLinesForward();
+		CreateNewLine();
+	}
+
+	void CreateNewLine(){
+		var line_clone = Instantiate (line,transform.position, transform.rotation) as GameObject;
+		line_clone.transform.parent = transform;
+		line_clone.transform.Translate(0,0,0);
+	}
+
+	void AllLinesForward(){
+		int lines = transform.childCount;
+		Debug.Log (lines);
+
+		for (int i = 0; i < lines; i++) {
+			Debug.Log ("now child at "+ i);
+			Transform child = transform.GetChild (i);
+			var c = child.gameObject;
+			Debug.Log (c);
+			c.GetComponent<SingleLine>().Forward();
+		}
 	}
 }
