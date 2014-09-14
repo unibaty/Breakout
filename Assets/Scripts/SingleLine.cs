858d2f9475d69903b6da411bacb30e3dee64a877
monpy <4696monpy@gmail.com>
2014-09-14 20:15:15 +0900

    １ライン用のC#を準備

---
+++ Assets/Scripts/SingleLine.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleLine : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.childCount == 0){
+			DestroyOwn();
+		}
+	}
+
+	void DestroyOwn(){
+		Destroy(gameObject);
+	}
+
+	public void Forward(){
+		transform.Translate(0,-1,0);
+	}
+}
